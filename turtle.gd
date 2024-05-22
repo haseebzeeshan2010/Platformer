49524a1709ef285e408d4a664dee91027a22aefa
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-05-22 15:18:43 +0100

    Keep movement at corelllating fps of animation

--- turtle.gd
+++ turtle.gd
@@ -1,25 +1,32 @@
 extends RigidBody2D
 
-var impulse_strength = 58
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+var impulse_strength = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var angle = rotation
+	var overlapping_mobs = %GroundCheck.get_overlapping_bodies()
 	
-	
+	if overlapping_mobs.size() > 2:
 
-	if Input.is_action_pressed("right_move"):
-		impulse_strength = 58
+
+		var angle = rotation
+		
+		%AnimatedSprite2D.set_speed_scale(linear_velocity.length()/1000)
+		
 		apply_central_impulse(Vector2(cos(angle), sin(angle)) * impulse_strength)
-		#apply_central_impulse(Vector2(10,0))
-	elif Input.is_action_pressed("left_move"):
-		impulse_strength = 58
-		apply_central_impulse(Vector2(cos(angle), sin(angle)) * -impulse_strength)
-	else:
-		impulse_strength = 0
-	pass
+		if Input.is_action_pressed("right_move"):
+			impulse_strength = 38
+			
+			
+			#apply_central_impulse(Vector2(10,0))
+		elif Input.is_action_pressed("left_move"):
+			impulse_strength = -38
+
+		else:
+			impulse_strength = 0 
+		
+		print(linear_velocity.length()/700)
+		
+		if Input.is_action_pressed("speed_bump"):
+			impulse_strength = 158
+		pass
